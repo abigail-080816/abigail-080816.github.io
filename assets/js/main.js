9e83746ab61beb9fd714b0e7f5275af048f8e766
abigail-080816 <brennabigail08@gmail.com>
2024-07-25 06:32:05 +0000

    before music, added video (may bug naredirect sa mega pag phone)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,211 @@
+// /*
+// 	Paradigm Shift by HTML5 UP
+// 	html5up.net | @ajlkn
+// 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+// */
+
+// (function($) {
+
+// 	var	$window = $(window),
+// 		$body = $('body');
+
+// 	// Breakpoints.
+// 		breakpoints({
+// 			default:   ['1681px',   null       ],
+// 			xlarge:    ['1281px',   '1680px'   ],
+// 			large:     ['981px',    '1280px'   ],
+// 			medium:    ['737px',    '980px'    ],
+// 			small:     ['481px',    '736px'    ],
+// 			xsmall:    ['361px',    '480px'    ],
+// 			xxsmall:   [null,       '360px'    ]
+// 		});
+
+// 	// Play initial animations on page load.
+// 		$window.on('load', function() {
+// 			window.setTimeout(function() {
+// 				$body.removeClass('is-preload');
+// 			}, 100);
+// 		});
+
+// 	// Hack: Enable IE workarounds.
+// 		if (browser.name == 'ie')
+// 			$body.addClass('is-ie');
+
+// 	// Mobile?
+// 		if (browser.mobile)
+// 			$body.addClass('is-mobile');
+
+// 	// Scrolly.
+// 		$('.scrolly')
+// 			.scrolly({
+// 				offset: 100
+// 			});
+
+// 	// Polyfill: Object fit.
+// 		if (!browser.canUse('object-fit')) {
+
+// 			$('.image[data-position]').each(function() {
+
+// 				var $this = $(this),
+// 					$img = $this.children('img');
+
+// 				// Apply img as background.
+// 					$this
+// 						.css('background-image', 'url("' + $img.attr('src') + '")')
+// 						.css('background-position', $this.data('position'))
+// 						.css('background-size', 'cover')
+// 						.css('background-repeat', 'no-repeat');
+
+// 				// Hide img.
+// 					$img
+// 						.css('opacity', '0');
+
+// 			});
+
+// 			$('.gallery > a').each(function() {
+
+// 				var $this = $(this),
+// 					$img = $this.children('img');
+
+// 				// Apply img as background.
+// 					$this
+// 						.css('background-image', 'url("' + $img.attr('src') + '")')
+// 						.css('background-position', 'center')
+// 						.css('background-size', 'cover')
+// 						.css('background-repeat', 'no-repeat');
+
+// 				// Hide img.
+// 					$img
+// 						.css('opacity', '0');
+
+// 			});
+
+// 		}
+
+// 	// Gallery.
+// 		$('.gallery')
+// 			.on('click', 'a', function(event) {
+
+// 				var $a = $(this),
+// 					$gallery = $a.parents('.gallery'),
+// 					$modal = $gallery.children('.modal'),
+// 					$modalImg = $modal.find('img'),
+// 					href = $a.attr('href');
+
+// 				// Not an image? Bail.
+// 					if (!href.match(/\.(jpg|gif|png|mp4)$/))
+// 						return;
+
+// 				// Prevent default.
+// 					event.preventDefault();
+// 					event.stopPropagation();
+
+// 				// Locked? Bail.
+// 					if ($modal[0]._locked)
+// 						return;
+
+// 				// Lock.
+// 					$modal[0]._locked = true;
+
+// 				// Set src.
+// 					$modalImg.attr('src', href);
+
+// 				// Set visible.
+// 					$modal.addClass('visible');
+
+// 				// Focus.
+// 					$modal.focus();
+
+// 				// Delay.
+// 					setTimeout(function() {
+
+// 						// Unlock.
+// 							$modal[0]._locked = false;
+
+// 					}, 600);
+
+// 			})
+// 			.on('click', '.modal', function(event) {
+
+// 				var $modal = $(this),
+// 					$modalImg = $modal.find('img');
+
+// 				// Locked? Bail.
+// 					if ($modal[0]._locked)
+// 						return;
+
+// 				// Already hidden? Bail.
+// 					if (!$modal.hasClass('visible'))
+// 						return;
+
+// 				// Stop propagation.
+// 					event.stopPropagation();
+
+// 				// Lock.
+// 					$modal[0]._locked = true;
+
+// 				// Clear visible, loaded.
+// 					$modal
+// 						.removeClass('loaded')
+
+// 				// Delay.
+// 					setTimeout(function() {
+
+// 						$modal
+// 							.removeClass('visible')
+
+// 						setTimeout(function() {
+
+// 							// Clear src.
+// 								$modalImg.attr('src', '');
+
+// 							// Unlock.
+// 								$modal[0]._locked = false;
+
+// 							// Focus.
+// 								$body.focus();
+
+// 						}, 475);
+
+// 					}, 125);
+
+// 			})
+// 			.on('keypress', '.modal', function(event) {
+
+// 				var $modal = $(this);
+
+// 				// Escape? Hide modal.
+// 					if (event.keyCode == 27)
+// 						$modal.trigger('click');
+
+// 			})
+// 			.on('mouseup mousedown mousemove', '.modal', function(event) {
+
+// 				// Stop propagation.
+// 					event.stopPropagation();
+
+// 			})
+// 			.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
+// 				.find('img')
+// 					.on('load', function(event) {
+
+// 						var $modalImg = $(this),
+// 							$modal = $modalImg.parents('.modal');
+
+// 						setTimeout(function() {
+
+// 							// No longer visible? Bail.
+// 								if (!$modal.hasClass('visible'))
+// 									return;
+
+// 							// Set loaded.
+// 								$modal.addClass('loaded');
+
+// 						}, 275);
+
+// 					});
+
+// })(jQuery);
 /*
 	Paradigm Shift by HTML5 UP
 	html5up.net | @ajlkn
@@ -10,199 +218,220 @@
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
+	breakpoints({
+		default:   ['1681px',   null       ],
+		xlarge:    ['1281px',   '1680px'   ],
+		large:     ['981px',    '1280px'   ],
+		medium:    ['737px',    '980px'    ],
+		small:     ['481px',    '736px'    ],
+		xsmall:    ['361px',    '480px'    ],
+		xxsmall:   [null,       '360px'    ]
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Hack: Enable IE workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
+	if (browser.name == 'ie')
+		$body.addClass('is-ie');
 
 	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
+	if (browser.mobile)
+		$body.addClass('is-mobile');
 
 	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				offset: 100
-			});
+	$('.scrolly')
+		.scrolly({
+			offset: 100
+		});
 
 	// Polyfill: Object fit.
-		if (!browser.canUse('object-fit')) {
+	if (!browser.canUse('object-fit')) {
 
-			$('.image[data-position]').each(function() {
+		$('.image[data-position]').each(function() {
 
-				var $this = $(this),
-					$img = $this.children('img');
+			var $this = $(this),
+				$img = $this.children('img');
 
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', $this.data('position'))
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
+			// Apply img as background.
+			$this
+				.css('background-image', 'url("' + $img.attr('src') + '")')
+				.css('background-position', $this.data('position'))
+				.css('background-size', 'cover')
+				.css('background-repeat', 'no-repeat');
 
-				// Hide img.
-					$img
-						.css('opacity', '0');
+			// Hide img.
+			$img.css('opacity', '0');
 
-			});
+		});
 
-			$('.gallery > a').each(function() {
+		$('.gallery > a').each(function() {
 
-				var $this = $(this),
-					$img = $this.children('img');
+			var $this = $(this),
+				$img = $this.children('img');
 
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', 'center')
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
+			// Apply img as background.
+			$this
+				.css('background-image', 'url("' + $img.attr('src') + '")')
+				.css('background-position', 'center')
+				.css('background-size', 'cover')
+				.css('background-repeat', 'no-repeat');
 
-				// Hide img.
-					$img
-						.css('opacity', '0');
+			// Hide img.
+			$img.css('opacity', '0');
 
-			});
+		});
 
-		}
+	}
 
 	// Gallery.
-		$('.gallery')
-			.on('click', 'a', function(event) {
+	$('.gallery')
+		.on('click', 'a', function(event) {
 
-				var $a = $(this),
-					$gallery = $a.parents('.gallery'),
-					$modal = $gallery.children('.modal'),
-					$modalImg = $modal.find('img'),
-					href = $a.attr('href');
+			var $a = $(this),
+				$gallery = $a.parents('.gallery'),
+				$modal = $gallery.children('.modal'),
+				$modalImg = $modal.find('img'),
+				$modalIframe = $modal.find('iframe'),
+				href = $a.attr('href');
 
-				// Not an image? Bail.
-					if (!href.match(/\.(jpg|gif|png|mp4)$/))
-						return;
+			// Not an image or video? Bail.
+			if (!href.match(/\.(jpg|gif|png|mp4)$/))
+				return;
 
-				// Prevent default.
-					event.preventDefault();
-					event.stopPropagation();
+			// Prevent default.
+			event.preventDefault();
+			event.stopPropagation();
 
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
+			// Locked? Bail.
+			if ($modal[0]._locked)
+				return;
 
-				// Lock.
-					$modal[0]._locked = true;
+			// Lock.
+			$modal[0]._locked = true;
 
-				// Set src.
-					$modalImg.attr('src', href);
+			// Set src for image or iframe.
+			if (href.match(/\.(mp4)$/)) {
+				$modalImg.hide();
+				$modalIframe.attr('src', href).show();
+			} else {
+				$modalIframe.hide();
+				$modalImg.attr('src', href).show();
+			}
 
-				// Set visible.
-					$modal.addClass('visible');
+			// Set visible.
+			$modal.addClass('visible');
 
-				// Focus.
-					$modal.focus();
+			// Focus.
+			$modal.focus();
 
-				// Delay.
-					setTimeout(function() {
+			// Delay.
+			setTimeout(function() {
+				// Unlock.
+				$modal[0]._locked = false;
+			}, 600);
 
-						// Unlock.
-							$modal[0]._locked = false;
+		})
+		.on('click', '.modal', function(event) {
 
-					}, 600);
+			var $modal = $(this),
+				$modalImg = $modal.find('img'),
+				$modalIframe = $modal.find('iframe');
 
-			})
-			.on('click', '.modal', function(event) {
+			// Locked? Bail.
+			if ($modal[0]._locked)
+				return;
 
-				var $modal = $(this),
-					$modalImg = $modal.find('img');
+			// Already hidden? Bail.
+			if (!$modal.hasClass('visible'))
+				return;
 
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
+			// Stop propagation.
+			event.stopPropagation();
 
-				// Already hidden? Bail.
-					if (!$modal.hasClass('visible'))
-						return;
+			// Lock.
+			$modal[0]._locked = true;
 
-				// Stop propagation.
-					event.stopPropagation();
+			// Clear visible, loaded.
+			$modal.removeClass('loaded');
 
-				// Lock.
-					$modal[0]._locked = true;
+			// Delay.
+			setTimeout(function() {
 
-				// Clear visible, loaded.
-					$modal
-						.removeClass('loaded')
+				$modal.removeClass('visible');
 
-				// Delay.
-					setTimeout(function() {
+				setTimeout(function() {
+					// Clear src.
+					$modalImg.attr('src', '');
+					$modalIframe.attr('src', '');
 
-						$modal
-							.removeClass('visible')
+					// Unlock.
+					$modal[0]._locked = false;
 
-						setTimeout(function() {
+					// Focus.
+					$body.focus();
 
-							// Clear src.
-								$modalImg.attr('src', '');
+				}, 475);
 
-							// Unlock.
-								$modal[0]._locked = false;
+			}, 125);
 
-							// Focus.
-								$body.focus();
+		})
+		.on('keypress', '.modal', function(event) {
 
-						}, 475);
+			var $modal = $(this);
 
-					}, 125);
+			// Escape? Hide modal.
+			if (event.keyCode == 27)
+				$modal.trigger('click');
 
-			})
-			.on('keypress', '.modal', function(event) {
+		})
+		.on('mouseup mousedown mousemove', '.modal', function(event) {
 
-				var $modal = $(this);
+			// Stop propagation.
+			event.stopPropagation();
 
-				// Escape? Hide modal.
-					if (event.keyCode == 27)
-						$modal.trigger('click');
+		})
+		.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /><iframe src="" frameborder="0" allowfullscreen></iframe></div></div>')
+		.find('img')
+		.on('load', function(event) {
 
-			})
-			.on('mouseup mousedown mousemove', '.modal', function(event) {
+			var $modalImg = $(this),
+				$modal = $modalImg.parents('.modal');
 
-				// Stop propagation.
-					event.stopPropagation();
+			setTimeout(function() {
 
-			})
-			.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
-				.find('img')
-					.on('load', function(event) {
+				// No longer visible? Bail.
+				if (!$modal.hasClass('visible'))
+					return;
 
-						var $modalImg = $(this),
-							$modal = $modalImg.parents('.modal');
+				// Set loaded.
+				$modal.addClass('loaded');
 
-						setTimeout(function() {
+			}, 275);
 
-							// No longer visible? Bail.
-								if (!$modal.hasClass('visible'))
-									return;
+		})
+		.end()
+		.find('iframe')
+		.on('load', function(event) {
 
-							// Set loaded.
-								$modal.addClass('loaded');
+			var $modalIframe = $(this),
+				$modal = $modalIframe.parents('.modal');
 
-						}, 275);
+			setTimeout(function() {
 
-					});
+				// No longer visible? Bail.
+				if (!$modal.hasClass('visible'))
+					return;
+
+				// Set loaded.
+				$modal.addClass('loaded');
+
+			}, 275);
+
+		});
 
 })(jQuery);
